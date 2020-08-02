bed68a6775be68ea4a15c98b8afa82aac80cf513
fune0 <fune0x8@gmail.com>
2020-08-02 15:27:59 +0900

    ログイン機能を作ろう：応用設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
   # サインインしていない場合はユーザをサインインページにリダイレクト
   before_action :authenticate_user!
+  # deviseに関連して特定のパラメーターを許可する
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # sign_inのときに、group_keyも許可する
+      devise_parameter_sanitizer.permit(:sign_in, keys:[:group_key])
+    # sign_upのときに、group_keyも許可する
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:group_key])
+    #account_updateのときに、group_keyも許可する
+      devise_parameter_sanitizer.permit(:account_update, keys:[:group_key])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_050919) do
+ActiveRecord::Schema.define(version: 2020_08_02_060119) do
+
+  create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "key"
+    t.text "detail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_08_02_050919) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "group_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
