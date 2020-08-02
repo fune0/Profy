0683f1c2536e86f0cbb47f72197c297ac90d1b74
fune0 <fune0x8@gmail.com>
2020-08-02 14:50:19 +0900

    ログイン機能を作ろう：基本設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  # サインインしていない場合はユーザをサインインページにリダイレクト
+  before_action :authenticate_user!
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,16 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.action_mailer.default_url_options = {host: 'http://localhost:3000/' }
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :enable_starttls_auto => true,
+  #   :address => "smtp.gmail.com",
+  #   :port => 587,
+  #   :domain => 'smtp.gmail.com',
+  #   :user_name => "fune0x8@example.com", #先ほど作成した送信用Gmailアドレス
+  #   :password => "", #2段階認証したアカウントで発行したアプリパスワード
+  #   :authentication => 'login'
+  # }
 end
